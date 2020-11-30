f7c0f258e7677df593c91a19f35cfdf1ebf505b0
haotran0903 <35643309+haotran0903@users.noreply.github.com>
2020-11-30 08:44:28 +0700

    UPDATE

--- backend/app/config/sequelize.js
+++ backend/app/config/sequelize.js
@@ -3,5 +3,11 @@ const db = require('./db.config.json')
 
 module.exports = new Sequelize(db.database, db.username, db.password, {
     host: db.host,
-    dialect: db.dialect
+    dialect: db.dialect,    
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
 })
