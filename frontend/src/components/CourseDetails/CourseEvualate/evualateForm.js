c89c61eee59922ee13b0ef3465b8c20fb9c9b953
haotran0903 <35643309+haotran0903@users.noreply.github.com>
2020-12-15 21:31:39 +0700

    UPDATE

--- frontend/src/components/CourseDetails/CourseEvualate/evualateForm.js
+++ frontend/src/components/CourseDetails/CourseEvualate/evualateForm.js
@@ -1,10 +1,5 @@
-import { Button, Checkbox, Drawer, FormControlLabel, Input } from "@material-ui/core";
-import { useEffect, useState } from "react";
-import { AiOutlineWarning } from "react-icons/ai";
+import { Button, Drawer, Input } from "@material-ui/core";
 import styled from "styled-components";
-import { getCourseOutcomeList } from "../../../api/CourseAPI";
-import { ErrorHelper } from "../../../utils";
-import { LoadingOverlayDiv } from "../../common/LoadingOverlay";
 import { LoadingOverlay } from "../../StatelessComponents";
 
 const Label = styled.label`
@@ -13,190 +8,124 @@ const Label = styled.label`
 `
 
 const EvualateForm = props => {
-    const [loading, setLoading] = useState(false)
-    const [outcomes, setOutcomes] = useState(null)
-
-    const handleChange = (e, data) => {
-        if (e.target.checked) {
-            props.setCdr(props.cdr.concat(data))
-        }
-        else {
-            props.setCdr(props.cdr.filter(ele => ele !== data))
-        }
-    }
-
-    useEffect(() => {
-        if (props.open) {
-            setLoading(true)
-            getCourseOutcomeList(props.mamh)
-                .then(data => {
-                    setLoading(false)
-                    setOutcomes(data)
-                })
-                .catch(err => {
-                    console.log(err)
-                    alert(ErrorHelper(err))
-                    props.setClose()
-                })
-        }
-    }, [props.open])
-
     return <Drawer
         open={props.open}
         anchor="right"
     >
         <div className="detail-form__wrapper">
-            {
-                (loading || !outcomes)
-                    ? <LoadingOverlayDiv />
-                    : <>
-                        {props.loading && <LoadingOverlayDiv />}
-                        <header className="detail-form__header">
-                            {props.header}
-                        </header>
-                        <div className="detail-form__main">
-                            {
-                                outcomes.length === 0
-                                    ? <div className="flex-center flex-column h-100">
-                                        <AiOutlineWarning className="text-danger" size="100px" />
-                                        <h3
-                                            style={{ fontSize: "25px" }}
-                                            className="section-title-color font-weight-bold m-0 mb-1"
-                                        >
-                                            Chưa có chuẩn đầu ra
-                                        </h3>
-                                        <p className="text-secondary">
-                                            Vui lòng tạo chuẩn đầu ra mới!
-                                        </p>
-                                    </div>
-                                    : <>
-                                        <div className="mt-2">
-                                            <Label for="hinhthuc">
-                                                Hình thức
-                                            </Label>
-                                            <Input
-                                                id="hinhthuc"
-                                                value={props.hinhthuc}
-                                                onChange={e => props.setHinhthuc(e.target.value)}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                            />
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="nhom">
-                                                Phân nhóm
-                                            </Label>
-                                            <Input
-                                                id="nhom"
-                                                value={props.phanloai}
-                                                onChange={e => props.setPhanloai(e.target.value)}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                            />
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="thoidiem">
-                                                Thời điểm
-                                            </Label>
-                                            <Input
-                                                id="thoidiem"
-                                                value={props.thoidiem}
-                                                onChange={e => props.setThoidiem(e.target.value)}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                            />
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="congcu_kt">
-                                                Công cụ KT
-                                            </Label>
-                                            <Input
-                                                id="congcu_kt"
-                                                value={props.congcu_kt}
-                                                onChange={e => props.setCongcuKT(e.target.value)}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                            />
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="cdr">
-                                                Chuẩn đầu ra
-                                            </Label>
-                                            <div>
-                                                {outcomes.map(ele => (
-                                                    <FormControlLabel
-                                                        key={ele.cdr}
-                                                        control={
-                                                            <Checkbox
-                                                                onChange={e => handleChange(e, ele)}
-                                                                name={ele.cdr}
-                                                                color="primary"
-                                                            />
-                                                        }
-                                                        label={ele.cdr}
-                                                        className="m-0 col-6"
-                                                    />
-                                                ))}
-                                            </div>
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="tile">
-                                                Tỉ lệ
-                                            </Label>
-                                            <Input
-                                                id="tile"
-                                                type="number"
-                                                value={props.tile}
-                                                onChange={e => props.setTile(e.target.value)}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                            />
-                                        </div>
-                                        <div className="mt-2">
-                                            <Label for="noidung">
-                                                Nội Dung
-                                            </Label>
-                                            <Input
-                                                id="noidung"
-                                                value={props.noidung}
-                                                onChange={e => props.setNoidung(e.target.value)}
-                                                rows={10}
-                                                rowsMax={255}
-                                                fullWidth
-                                                disableUnderline
-                                                inputProps={{ className: "grey-200-bg border p-2 rounded" }}
-                                                multiline
-                                            />
-                                        </div>
-
-                                    </>
-                            }
-                        </div>
-                        <div className="detail-form__footer">
-                            <Button
-                                className="light-blue-bgcolor"
-                                color="primary"
-                                variant="contained"
-                                onClick={props.handleSubmit}
-                            >
-                                {props.edit ? "Chỉnh sửa" : "Tạo mới"}
-                            </Button>
-                            <Button
-                                className="ml-3"
-                                color="secondary"
-                                variant="outlined"
-                                onClick={props.setClose}
-                            >
-                                Đóng
-                            </Button>
-                        </div>
-                    </>
-            }
+            {props.loading && <LoadingOverlay />}
+            <header className="detail-form__header">
+                {props.header}
+            </header>
+            <div className="detail-form__main">
+                <div className="mt-2">
+                    <Label for="hinhthuc">
+                        Hình Thức
+                    </Label>
+                    <Input
+                        id="hinhthuc"
+                        disabled={props.edit}
+                        value={props.hinhthuc}
+                        onChange={e => props.setHinhthuc(e.target.value)}
+                        fullWidth
+                        disableUnderline
+                        inputProps={{
+                            style: {
+                                fontFamily: "Open Sans"
+                            },
+                            className: "grey-200-bg border p-2 rounded"
+                        }}
+                    />
+                </div>
+                <div className="mt-2">
+                    <Label for="noidung">
+                        Nội Dung
+                    </Label>
+                    <Input
+                        id="noidung"
+                        value={props.noidung}
+                        onChange={e => props.setNoidung(e.target.value)}
+                        rows={10}
+                        rowsMax={255}
+                        fullWidth
+                        disableUnderline
+                        inputProps={{ className: "grey-200-bg border p-2 rounded" }}
+                        multiline
+                    />
+                </div>
+                <div className="mt-2">
+                    <Label for="congcu_kt">
+                        Công cụ KT
+                    </Label>
+                    <Input
+                        id="congcu_kt"
+                        value={props.congcu_kt}
+                        onChange={e => props.setcongcu_kt(e.target.value)}                 
+                        inputProps={{ className: "grey-200-bg border p-2 rounded" }}
+                        fullWidth
+                        disableUnderline                      
+                    />
+                </div>
+                <div className="mt-2">
+                    <Label for="thoidiem">
+                        Thời Điểm
+                    </Label>
+                    <Input
+                        id="thoidiem"
+                        value={props.thoidiem}
+                        onChange={e => props.setthoidiem(e.target.value)}                 
+                        inputProps={{ className: "grey-200-bg border p-2 rounded" }}
+                        fullWidth
+                        disableUnderline                      
+                    />
+                </div>
+                <div className="mt-2">
+                    <Label for="cdr_kt">
+                        Chuẩn Đầu Ra KT
+                    </Label>
+                    <Input
+                        id="cdr_kt"
+                        value={props.cdr_kt}
+                        onChange={e => props.setcdr_kt(e.target.value)}                 
+                        inputProps={{ className: "grey-200-bg border p-2 rounded" }}
+                        fullWidth
+                        disableUnderline                      
+                    />
+                </div>
+                <div className="mt-2">
+                    <Label for="tile">
+                        Tỉ Lệ
+                    </Label>
+                    <Input
+                        id="tile"
+                        value={props.tile}
+                        onChange={e => props.settile(e.target.value)}                 
+                        inputProps={{ className: "grey-200-bg border p-2 rounded" }}
+                        fullWidth
+                        disableUnderline                      
+                    />
+                </div>
+            </div>
+            <div className="detail-form__footer">
+                <Button
+                    className="text-transform-none light-blue-bgcolor"
+                    color="primary"
+                    variant="contained"
+                    onClick={props.handleSubmit}
+                    disabled={!props.hinhthuc || !props.noidung || !props.congcu_kt || !props.thoidiem ||
+                    !props.cdr_kt || !props.tile}
+                >
+                    {props.edit ? "Chỉnh sửa" : "Tạo mới"}
+                </Button>
+                <Button
+                    className="text-transform-none ml-3"
+                    color="secondary"
+                    variant="outlined"
+                    onClick={props.setClose}
+                >
+                    Đóng
+                </Button>
+            </div>
         </div>
     </Drawer>
 }
